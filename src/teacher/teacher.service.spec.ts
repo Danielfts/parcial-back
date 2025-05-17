0608ea2f3c03d1d0e64cb915f2c17a7c38200b96
Danielfts <d.trivino@uniandes.edu.co>
2025-05-17 17:57:50 -0500

    Completed create teacher tests

--- src/teacher/teacher.service.spec.ts
+++ src/teacher/teacher.service.spec.ts
@@ -4,6 +4,9 @@ import { Teacher } from './entities/teacher.entity';
 import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing/typeorm-testing-config';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateTeacherDto } from './dto/create-teacher.dto';
+import { faker } from '@faker-js/faker';
+import { BadRequestException } from '@nestjs/common';
 
 describe('TeacherService', () => {
   let service: TeacherService;
@@ -21,5 +24,38 @@ describe('TeacherService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should cretate a teacher', async () => {
+    const teacherDto: CreateTeacherDto = {
+      nombre: faker.person.fullName(),
+      extension: 99999,
+      numeroCedula: 9999999999,
+      esParEvaluador: faker.datatype.boolean(),
+      departamento: faker.lorem.word(),
+    };
+    const createdTeacher = await service.crearProfesor(teacherDto);
+    const teacherExists = await repository.existsBy({ id: createdTeacher.id });
+    expect(teacherExists).toBe(true);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, createdAt, updatedAt, mentorias, tests, ...teacherData } =
+      createdTeacher;
+    expect(teacherData).toEqual(teacherDto);
+  });
+
+  it('should not create a teacher with a 10 digit extension', async () => {
+    const teacherDto: CreateTeacherDto = {
+      nombre: faker.person.fullName(),
+      extension: 999999,
+      numeroCedula: 9999999999,
+      esParEvaluador: faker.datatype.boolean(),
+      departamento: faker.lorem.word(),
+    };
+    try {
+      await service.crearProfesor(teacherDto);
+      fail('The expected error was not thrown');
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+    }
   });
 });

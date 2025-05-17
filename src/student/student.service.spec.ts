b32097a710df0c523a28f034bd0c54965c68b686
Danielfts <d.trivino@uniandes.edu.co>
2025-05-17 13:54:06 -0500

    Completed more test cases

--- src/student/student.service.spec.ts
+++ src/student/student.service.spec.ts
@@ -6,13 +6,23 @@ import { TypeOrmTestingConfig } from '../shared/testing/typeorm-testing-config';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { faker } from '@faker-js/faker';
 import { CreateStudentDto } from './dto/create-student.dto';
+import { BadRequestException } from '@nestjs/common';
+import { Project } from '../project/entities/project.entity';
+import { Teacher } from '../teacher/entities/teacher.entity';
 
 describe('StudentService', () => {
   let service: StudentService;
-  let repository: Repository<Student>;
+  let studentRepository: Repository<Student>;
+  let projectRepository: Repository<Project>;
+  let teacherRepository: Repository<Teacher>;
 
-  const seedDatabase = async () => {
-    await repository.clear();
+  const clearDatabase = async () => {
+    const clearPromises = [
+      studentRepository.clear(),
+      projectRepository.clear(),
+      teacherRepository.clear(),
+    ];
+    await Promise.all(clearPromises);
   };
 
   beforeEach(async () => {
@@ -22,8 +32,16 @@ describe('StudentService', () => {
     }).compile();
 
     service = module.get<StudentService>(StudentService);
-    repository = module.get<Repository<Student>>(getRepositoryToken(Student));
-    await seedDatabase();
+    studentRepository = module.get<Repository<Student>>(
+      getRepositoryToken(Student),
+    );
+    projectRepository = module.get<Repository<Project>>(
+      getRepositoryToken(Project),
+    );
+    teacherRepository = module.get<Repository<Teacher>>(
+      getRepositoryToken(Teacher),
+    );
+    await clearDatabase();
   });
 
   it('should be defined', () => {
@@ -53,13 +71,30 @@ describe('StudentService', () => {
 
   it('should delete a student', async () => {
     const studentDto: CreateStudentDto = generateStudent();
-    const studentEntity = repository.create(studentDto);
-    const createdStudent = await repository.save(studentEntity);
+    const studentEntity = studentRepository.create(studentDto);
+    const createdStudent = await studentRepository.save(studentEntity);
     const id = createdStudent.id;
-    let exists = await repository.exists({ where: { id } });
+    let exists = await studentRepository.exists({ where: { id } });
     expect(exists).toBe(true);
     await service.eliminarEstudiante(id);
-    exists = await repository.exists({ where: { id } });
+    exists = await studentRepository.exists({ where: { id } });
     expect(exists).toBe(false);
   });
+
+  it('should not create a student with low grades', async () => {
+    const studentDto: CreateStudentDto = generateStudent();
+    studentDto.promedio = 2.5;
+    await expect(service.crearEstudiante(studentDto)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('should not create a student from a semester lower than 4', async () => {
+    const studentDto: CreateStudentDto = generateStudent();
+    studentDto.semestre = 3;
+    await expect(service.crearEstudiante(studentDto)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+  // TODO define should not delete student with projects test
 });

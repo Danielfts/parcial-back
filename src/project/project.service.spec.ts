f12823c6de992f3b737fcbb89dbafa7190596e41
Danielfts <d.trivino@uniandes.edu.co>
2025-05-18 11:26:52 -0500

    Completed successfull create test

--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectService } from './project.service';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
-import { Teacher } from '../teacher/entities/teacher.entity';
-import { Student } from '../student/entities/student.entity';
 import { TypeOrmTestingConfig } from '../shared/testing/typeorm-testing-config';
 import { Repository } from 'typeorm';
+import { Student } from '../student/entities/student.entity';
+import { Teacher } from '../teacher/entities/teacher.entity';
+import { faker } from '@faker-js/faker/.';
+import { CreateProjectDto } from './dto/create-project.dto';
 
 describe('ProjectService', () => {
   let service: ProjectService;
-  let repository: Repository<Project>;
-
+  let projectRepository: Repository<Project>;
+  let studentRepository: Repository<Student>;
+  let teacherRepository: Repository<Teacher>;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [...TypeOrmTestingConfig()],
@@ -18,10 +21,107 @@ describe('ProjectService', () => {
     }).compile();
 
     service = module.get<ProjectService>(ProjectService);
-    repository = module.get<Repository<Project>>(getRepositoryToken(Project));
+    projectRepository = module.get<Repository<Project>>(
+      getRepositoryToken(Project),
+    );
+    studentRepository = module.get<Repository<Student>>(
+      getRepositoryToken(Student),
+    );
+    teacherRepository = module.get<Repository<Teacher>>(
+      getRepositoryToken(Teacher),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // Create project: positive tests
+  // Budget > 0
+  // length of title > 15
+  it('should create a project', async () => {
+    // preconditions for creating a project: a student and a teacher
+    const { student, teacher } = await seedDatabase();
+    const projectDto: CreateProjectDto = {
+      ...generateProject(),
+      mentorId: teacher.id,
+      studentId: student.id,
+    };
+    const project = await service.crearProyecto(projectDto);
+    const savedProject = await projectRepository.findOneOrFail({
+      where: { id: project.id },
+      relations: ['mentor', 'lider'],
+    });
+    const {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      id,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      createdAt,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      updatedAt,
+      lider: savedLider,
+      mentor: savedMentor,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      tests,
+      ...projectData
+    } = savedProject;
+    expect({
+      ...projectData,
+      mentorId: savedMentor.id,
+      studentId: savedLider!.id,
+    }).toEqual(projectDto);
+  });
+  // Create project: negative tests
+  it('should not create a project with a negative budget', () => {});
+
+  it('should not create a project with a title with length <= 15', () => {});
+  // Advance project: positive tests
+  // state: integer between 0 and 4
+  it('should advance a project', () => {});
+  // Advance project: negative tests
+  it('should not advance a project to a value beyond 4', () => {});
+  // Find all estudiantes: positive tests
+  it('should find all students that work on a project', () => {});
+  // Find all estudiantes: negative tests
+  it('should not find students for a non existing project', () => {});
+
+  // helper functions
+  function generateNDigitNumber(digits: number): number {
+    const min = Math.pow(10, digits - 1);
+    const max = Math.pow(10, digits) - 1;
+    return faker.number.int({ min, max });
+  }
+
+  const generateProject = () => {
+    const projectDto = {
+      titulo: faker.lorem.words(),
+      estado: 0,
+      presupuesto: faker.number.int(100),
+      notaFinal: faker.number.float(5),
+      fechaInicio: new Date().toISOString(),
+      fechaFin: new Date().toISOString(),
+      area: faker.lorem.word(),
+    };
+    return projectDto;
+  };
+
+  const seedDatabase = async () => {
+    // create a student
+    const student: Student = await studentRepository.save({
+      nombre: faker.person.fullName(),
+      numeroCedula: generateNDigitNumber(10),
+      programa: faker.lorem.word(),
+      promedio: faker.number.float(5),
+      semestre: faker.number.int(10),
+    });
+    // create a teacher
+    const teacher: Teacher = await teacherRepository.save({
+      nombre: faker.person.fullName(),
+      numeroCedula: generateNDigitNumber(10),
+      extension: generateNDigitNumber(5),
+      departamento: faker.lorem.word(),
+      esParEvaluador: faker.datatype.boolean(),
+    });
+    return { student, teacher };
+  };
 });
